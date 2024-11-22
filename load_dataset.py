2e4f3be9b6cd3a541354fe005cb7a7a1c55190b4
Gcladera <graucladerasensat@gmail.com>
2024-11-22 17:03:31 +0100

    Prova kagglehub

--- load_dataset.py
+++ load_dataset.py
@@ -1,2 +1,9 @@
 def load_dataset():
     return
+
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("obulisainaren/multi-cancer/multi cancer/Cervical Cancer")
+
+print("Path to dataset files:", path)
