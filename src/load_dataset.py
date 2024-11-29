fbcde1b406b1baaa620f6a7f21a407dc6b8f774c
davividaboti <davividaboti8@gmail.com>
2024-11-29 17:14:19 +0100

    format dades definitiu

--- src/load_dataset.py
+++ src/load_dataset.py
@@ -1,72 +1,74 @@
 import os
 import cv2
 import pickle
-import numpy as np
 from sklearn.model_selection import train_test_split
-
+from sklearn.preprocessing import LabelEncoder
+from collections import defaultdict
+import numpy as np
 
 def load_dataset(path):
     try:
         with open('data/dataset.pkl', 'rb') as f:
-            dataset = pickle.load(f)
+            dataset, labels = pickle.load(f)
     except:
-
-        dataset = {}
+        dataset = []
+        labels = []
         for root, dirs, files in os.walk(path):
-            for dir_name in dirs:
+            for dir_name in dirs[:3]:  # Only take the first 3 directories
                 folder_path = os.path.join(root, dir_name)
                 
-                images = [os.path.join(folder_path, file) for file in os.listdir(folder_path)
-                    if cv2.imread(os.path.join(folder_path, file)) is not None]
-                dataset[dir_name] = images
+                images = []
+                for file in os.listdir(folder_path)[:200]:  # Only take the first 200 images
+                    img_path = os.path.join(folder_path, file)
+                    img = cv2.imread(img_path)
+                    if img is not None:
+                        img_resized = img[::10, ::10]  # Take one pixel every 10 pixels
+                        dataset.append(img_resized)
+                        labels.append(dir_name)
         with open('data/dataset.pkl', 'wb') as f:
             pickle.dump(dataset, f)
-    return dataset
+    return dataset, labels
 
-def preprocess_images(image_paths, target_size=(128, 128)):
+def preprocess_images(image_paths, target_size=(32, 32)):
     images = []
-    for path in image_paths:
-        try:
-            img = cv2.imread(path)
-            if img is not None:
-                img = cv2.resize(img, target_size)
-                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-                img = img / 255.0
-                images.append(img)
-        except Exception as e:
-            print(f"Error processant {path}: {e}")
-    return np.array(images)
+    categories = {k:v for k, v in enumerate(image_paths.keys())}
+    for category in image_paths.values():
+        for path in category[:200]:
+            try:
+                img = cv2.imread(path)
+                if img is not None:
+                    img = cv2.resize(img, target_size)
+                    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                    img = img / 255.0
+                    images.append(img)
+            except Exception as e:
+                print(f"Error processant {path}: {e}")
+    return np.array(images), categories
 
 
-def train_test(dataset, train_size=0.6, test_size=0.2, val_size=0.2):
-    X_train, y_train = [], []
-    X_val, y_val = [], []
-    X_test, y_test = [], []
-    
-    for class_name, images in dataset.items():
-        # Dividir el conjunt entre dades d'entrenament i (validació + test)
-        train_images, temp_images = train_test_split(images, train_size=train_size, random_state=42)
+def train_test(dataset, labels, test_size=0.2, val_size=0.2):
+    X_train, X_val_test, y_train, y_val_test = train_test_split(dataset, labels, train_size=val_size+test_size, random_state=42)
+    # print(len(X_train), len(y_train), len(X_val_test), len(y_val_test))
+    X_test, X_val, y_test, y_val = train_test_split(X_val_test, y_val_test, test_size=val_size/(test_size + val_size), random_state=42)
+    # ore = LabelEncoder(list(dataset.keys()))
+    # y_train_encoded = ore.fit_transform(y_train)
+    # y_test_encoded = ore.transform(y_test)
+    # y_val_encoded = ore.transform(y_val)
 
-        # Dividir el conjunt (validació + test) entre dades de validació i test
-        val_images, test_images = train_test_split(temp_images, test_size=test_size / (val_size + test_size), random_state=42)
-        
-        X_train.extend(train_images)
-        y_train.extend([class_name] * len(train_images))
-        
-        X_val.extend(val_images)
-        y_val.extend([class_name] * len(val_images))
-        
-        X_test.extend(test_images)
-        y_test.extend([class_name] * len(test_images))
-    
-    return X_train, y_train, X_val, y_val, X_test, y_test
+    encoders = LabelEncoder()
+    encoders.fit(list(set(y_train+y_test+y_val)))
+    # Encoding the variable
+    y_train_encoded = encoders.transform(y_train) #Per tranformar els str a numeros
+    y_test_encoded = encoders.transform(y_test)
+    y_val_encoded = encoders.transform(y_val)
+
+    return X_train, y_train_encoded, X_val, y_val_encoded, X_test, y_test_encoded
 
 
 
+#data, labels = load_dataset('data/Cervical_Cancer')
+#print(len(data), len(labels))
 
-# data = load_dataset('data/Cervical_Cancer')
-# X_train, y_train, X_val, y_val, X_test, y_test = train_test(data)
-# for x, y in zip(X_train, y_train):
-#     print("Imatge:", x, "Tipus de cancer:", y)
-
-    
+#X_train, y_train, X_val, y_val, X_test, y_test = train_test(data, labels)
+#for x, y in zip(X_train, y_train):
+#    print("Imatge:", x, "Tipus de cancer:", y)
