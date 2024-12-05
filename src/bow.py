de307dc85edfbaa26adce32430e68238aaeddaa3
ikersc9 <iker2004sc@gmail.com>
2024-12-05 13:32:50 +0100

    Correcio bow

--- src/bow.py
+++ src/bow.py
@@ -12,40 +12,37 @@ def train_visual_words(vector_features, n_clusters=1024):
     kmeans.fit(np.array(vector_features))
     return kmeans
 
-def bag_of_words_histogram(features, n_clusters=1024):
+def bag_of_words_histogram(vector, features, n_clusters=1024, sift=True):
     # Create the bag of words
     #dubte sobre si bow ha de ser una matriu o ha de ser una llista de matrius unidimensionals
-    try: 
-        with open("data/bow.pkl", "rb") as f:
-            return pickle.load(f)
-    except:
-        kmeans = train_visual_words(features, n_clusters)
-        bow = np.zeros((len(features), kmeans.n_clusters))
-        for label, image_feature in features.items():
-            for descriptor in image_feature:
-                print(descriptor)
-                pred = kmeans.predict(descriptor)
-                for i in pred:
-                    bow[label][i] += 1
-        return bow
+    kmeans = train_visual_words(vector, n_clusters)
+    bow = np.zeros((len(features), kmeans.n_clusters))
+    for label, image_feature in features.items():
+        for descriptor in image_feature:
+            print(descriptor)
+            pred = kmeans.predict(descriptor)
+            for i in pred:
+                bow[label][i] += 1
+    if sift:
+        with open("data/bow_sift.pkl", "wb") as f:
+            pickle.dump(bow, f)
+    else:
+        with open("data/bow_dense.pkl", "wb") as f:
+            pickle.dump(bow, f)
+    return bow
 
 def main():
     data, labels = load_dataset('data/Cervical_Cancer')
     labels_encoded = encode_labels(labels)
-    # try:
-    #     with open('data/features.pkl', 'rb') as f:
-    #         pass
-    #         # categories = pickle.load(f)
-    # except:
-    #     # vector, categories = extract_sift_features(data, labels, 128, None)
-    #     with open('data/features.pkl', 'wb') as f:
-    #         # pickle.dump(categories, f)
-    #         pass
-    #vector, categories = extract_sift_features(data, labels_encoded, 128, None)
 
-    features = extract_sift_features(data, labels_encoded, 128, None)
-    bow = bag_of_words_histogram(features)
-    print(bow)
+    try:
+        with open("data/bow_sift.pkl", "rb") as f:
+            bow = pickle.load(f)
+            print(bow)
+    except:
+        vectors, features = extract_sift_features(data, labels_encoded, 128, None)
+        bow = bag_of_words_histogram(vectors, features)
+        print(bow)
 
 
 
