c40780ab0cab3bd6cca2a1f8b463938c2f32e4aa
Gcladera <1673102@uab.cat>
2024-12-28 16:38:06 +0100

    randomized search

--- src/Primer model/train_test.py
+++ src/Primer model/train_test.py
@@ -4,7 +4,7 @@ from dense_sampling import *
 from sklearn import linear_model, svm
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV, cross_val_score
 from sklearn.ensemble import RandomForestClassifier
 # from sklearn.preprocessing import LabelEncoder
 import numpy as np
@@ -19,8 +19,21 @@ def grid_search(X_train, y_train, model, parameters):
     grid_search_cv.fit(X_train, y_train)
     return grid_search_cv.best_params_, grid_search_cv.best_estimator_
 
-def train_logistic_regression(X_train, y_train, c=0.1, solver="newton-cg", max_iter=5000, penalty="l2", classificador="ovr"):
-    c_values = [0.001, 0.01, 0.1, 1, 10, 100]
+def randomized_search(X_train, y_train, model, parameters, n_iter=50):
+    random_search_cv = RandomizedSearchCV(
+        estimator=model,
+        param_distributions=parameters,
+        n_iter=n_iter,  # Número de combinaciones aleatorias a probar
+        cv=3,           # Número de folds en la validación cruzada
+        n_jobs=-1,       # Usa todos los núcleos disponibles
+        random_state=42, # Semilla para reproducibilidad
+        verbose=2        # Nivel de detalle en la salida
+    )
+    random_search_cv.fit(X_train, y_train)
+    return random_search_cv.best_params_, random_search_cv.best_estimator_
+
+def train_logistic_regression(X_train, y_train, c=0.1, solver="newton-cg", max_iter=5000, penalty="l2", classificador="ovo"):
+    c_values = [0.01, 0.1, 1, 10]
     # c_values = [0.1]
     parameters = {'C': c_values, 'solver': ['lbfgs', 'liblinear', 'newton-cg', 'saga'], 'penalty':["l2"]}
     # parameters = {'C': c_values, 'solver': ['liblinear'], 'max_iter': [1000], 'penalty':["l2"]}
@@ -51,12 +64,15 @@ def train_random_forest(X_train, y_train, n_estimators=100, max_depth=None, clas
 
 def train_xgboost(X_train, y_train, classificador="ovr"):
     num_class = len(np.unique(y_train))
-    parameters = {'max_depth': [3, 6, 10], 'n_estimators': [100, 200, 300], 'learning_rate': [0.01, 0.05, 0.1], 'eval_metric': ['mlogloss'], "lambda": [0, 0.1, 1], "alpha": [0, 0.1, 1], "objective":["multi:softmax"]}
-    xgb_model = xgb.XGBClassifier(random_state=42, num_class=num_class)
-    best_params, model = grid_search(X_train, y_train, xgb_model, parameters)
+    # parameters = {'max_depth': [3, 6, 10], 'n_estimators': [100, 200, 300], 'learning_rate': [0.01, 0.05, 0.1], 'eval_metric': ['mlogloss'], "lambda": [0, 0.1, 1], "alpha": [0, 0.1, 1], "objective":["multi:softmax"]}
+    parameters = {'max_depth': 6, 'n_estimators': 200, 'learning_rate': 0.05, 'eval_metric': 'mlogloss', "lambda": 0.1, "alpha": 0.1, "objective":"multi:softmax", "random_state":42, "num_class":num_class}
+
+    xgb_model = xgb.XGBClassifier(max_depth=6, n_estimators= 200, learning_rate= 0.05, eval_metric= 'mlogloss', objective="multi:softmax", random_state=42, num_class=num_class)
+    model = xgb_model
+    # best_params, model = randomized_search(X_train, y_train, xgb_model, parameters)
     model = OneVsRestClassifier(model) if classificador == "ovr" else OneVsOneClassifier(model)
     model.fit(X_train, y_train)
-    return model, best_params
+    return model, None#, best_params
 
 
 def prediccio_tests(model, X_test, y_test):
