6040eeafd3cbcffd83551b6544986a92e068ec29
davividaboti <davividaboti8@gmail.com>
2025-01-01 16:58:16 +0100

    neteja controller copy

--- src/Arxius addicionals/controller copy.py
+++ src/Arxius addicionals/controller copy.py
@@ -18,7 +18,7 @@ from visualitzacions import show_roc_curve
 
 
 def main():
-    wandb.login(key="e04f0be8ca1d2a05e435577f94aad6b2d9ad31bb")
+    wandb.login(key="ed5372ac56774e54e4d1a252caf37833454f5fae")
     # models = ["logistic", "svc", "random_forest", "xgboost"]
     models = ["xgboost"]
     detector = "sift" #"dense"
@@ -102,62 +102,62 @@ def main():
                 print("Recall: ", recall)
                 print("F1: ", f1)
 
-    # elif detector == "dense":
-    #     sift  = cv2.xfeatures2d.SIFT_create()
+    elif detector == "dense":
+        sift  = cv2.xfeatures2d.SIFT_create()
 
-    #     wandb.init(project="Practica2_DenseSampling",
-    #            config={
-    #                 "model": model,
-    #                 "features": "Dense Sampling",
-    #                 "pases": pases,
-    #                 "amplada_punt": amplada_punt,
-    #                 "n_clusters": n_clusters,
-    #                 "num_dades": num_dades,
-    #                 "num_directoris": num_directoris,
-    #                 "test_size": test_size,
-    #                 "val_size": val_size
-    #            })
+        wandb.init(project="Practica2_DenseSampling",
+               config={
+                    "model": model,
+                    "features": "Dense Sampling",
+                    "pases": pases,
+                    "amplada_punt": amplada_punt,
+                    "n_clusters": n_clusters,
+                    "num_dades": num_dades,
+                    "num_directoris": num_directoris,
+                    "test_size": test_size,
+                    "val_size": val_size
+               })
         
-    #     vectors_train, train_features = dense_sampling(sift, X_train, y_train, pases, amplada_punt)
-    #     _, val_features = dense_sampling(sift, X_test, y_test, pases, amplada_punt)
+        vectors_train, train_features = dense_sampling(sift, X_train, y_train, pases, amplada_punt)
+        _, val_features = dense_sampling(sift, X_test, y_test, pases, amplada_punt)
 
-    #     kmeans = train_visual_words(vectors_train, n_clusters)
+        kmeans = train_visual_words(vectors_train, n_clusters)
         
-    #     bow_train, labels_train = bag_of_words_histogram(train_features, kmeans)
-    #     bow_test, labels_test = bag_of_words_histogram(val_features, kmeans)
+        bow_train, labels_train = bag_of_words_histogram(train_features, kmeans)
+        bow_test, labels_test = bag_of_words_histogram(val_features, kmeans)
 
-        # with open("data/bow_sift_train.pkl", 'wb') as f:
-        #     pickle.dump(bow_train, f)
-        # with open("data/bow_sift_test.pkl", 'wb') as f:
-        #     pickle.dump(bow_test, f)
+        with open("data/bow_sift_train.pkl", 'wb') as f:
+            pickle.dump(bow_train, f)
+        with open("data/bow_sift_test.pkl", 'wb') as f:
+            pickle.dump(bow_test, f)
 
-    # print("Entrenant el model...")
-    # # print(len(bow_train), len(y_train))
-    # # model, best_params = train_logistic_regression(bow_train, labels_train)
-    # if model == "svc":
-    #     model, best_params = train_svc(bow_train, labels_train)
-    # elif model == "logistic":
-    #     model, best_params = train_logistic_regression(bow_train, labels_train)
-    # elif model == "random_forest":
-    #     model, best_params = train_random_forest(bow_train, labels_train)
-    # elif model == "xgboost":
-    #     model, best_params = train_xgboost(bow_train, labels_train)
-    # prediccio = model.predict(bow_test)
-    # print("confusion matrix: ",metrics.confusion_matrix(labels_test, prediccio))
-    # print("accuracy: ",metrics.accuracy_score(labels_test, prediccio))
+    print("Entrenant el model...")
+    # print(len(bow_train), len(y_train))
+    # model, best_params = train_logistic_regression(bow_train, labels_train)
+    if model == "svc":
+        model, best_params = train_svc(bow_train, labels_train)
+    elif model == "logistic":
+        model, best_params = train_logistic_regression(bow_train, labels_train)
+    elif model == "random_forest":
+        model, best_params = train_random_forest(bow_train, labels_train)
+    elif model == "xgboost":
+        model, best_params = train_xgboost(bow_train, labels_train)
+    prediccio = model.predict(bow_test)
+    print("confusion matrix: ",metrics.confusion_matrix(labels_test, prediccio))
+    print("accuracy: ",metrics.accuracy_score(labels_test, prediccio))
 
-    # print("Params: ", best_params)
-    # overfitting = model.score(bow_train, labels_train)
-    # print("Overfitting = ", overfitting)
-    # print("Resultats test = ", model.score(bow_test, labels_test))
-    # print("Visualitzant les mètriques...")
-    # accuracy, precision, recall, f1 = get_metrics(model, bow_test, labels_test)
-    # wandb.log({"overfitting":overfitting, "accuracy": accuracy, "precision": precision, "recall": recall, "f1": f1})
-    # wandb.finish()
-    # print("Accuracy: ", accuracy)
-    # print("Precision: ", precision)
-    # print("Recall: ", recall)
-    # print("F1: ", f1)
+    print("Params: ", best_params)
+    overfitting = model.score(bow_train, labels_train)
+    print("Overfitting = ", overfitting)
+    print("Resultats test = ", model.score(bow_test, labels_test))
+    print("Visualitzant les mètriques...")
+    accuracy, precision, recall, f1 = get_metrics(model, bow_test, labels_test)
+    wandb.log({"overfitting":overfitting, "accuracy": accuracy, "precision": precision, "recall": recall, "f1": f1})
+    wandb.finish()
+    print("Accuracy: ", accuracy)
+    print("Precision: ", precision)
+    print("Recall: ", recall)
+    print("F1: ", f1)
 
 if __name__=="__main__":
     main()
